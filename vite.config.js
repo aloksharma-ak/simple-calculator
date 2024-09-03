401f1d6b8a7f7a8d2e35c90c33d9c090d99a1cc1
aloksharma-ak <ak7570384742@gmail.com>
2024-09-03 16:19:51 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://aloksharma-ak.github.io/simple-calculator",
-  plugins: [react()],
+  base: '/simple-calculator/',
+  plugins: [react()]
 });
